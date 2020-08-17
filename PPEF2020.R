47b316710aeeccafa05541b5a5365507daa434f8
Ivan Paredes <ivanresendiz25@gmail.com>
2020-08-17 17:20:33 -0500

    Local change

--- PPEF2020.R
+++ PPEF2020.R
@@ -1,11 +1,5 @@
 library(readxl)
-#https://www.ppef.hacienda.gob.mx/work/models/PPEF2020/analiticosPresupuestarios/Proyecto/ac01_ra_ur_og.xlsx
-#Análisis por Unidad Responsable
-URBase <- read_excel("ac01_ra_ur_og.xlsx",
-range = "A12:F86279")
-
 library(zoo)
-View(URBase)
 
 #generar función para repetir nombres
 
@@ -37,15 +31,81 @@ fill.NAs <- function(isNA){
   replacement
 }
 
-#Sustituir NA con valorprevio en Ramo
-isNA <- as.numeric(is.na(URBase$RAMO))
+#############
+#2015 - 2020
+PEF2015 <- read_excel("2020-2016/ac01_ra_ur_og PEF2015.xlsx", col_names = TRUE, skip = 11)
+PEF2016 <- read_excel("2020-2016/ac01_ra_ur_og PEF2016.xlsx", col_names = TRUE, skip = 11)
+PEF2016 <- read_excel("2020-2016/ac01_ra_ur_og PEF2016.xlsx", col_names = TRUE, skip = 11)
+PEF2017 <- read_excel("2020-2016/ac01_ra_ur_og PEF2017.xlsx", col_names = TRUE, skip = 11)
+PEF2018 <- read_excel("2020-2016/ac01_ra_ur_og PEF2018.xlsx", col_names = TRUE, skip = 11)
+PEF2019 <- read_excel("2020-2016/ac01_ra_ur_og PEF2019.xlsx", col_names = TRUE, skip = 11)
+PEF2020 <- read_excel("2020-2016/ac01_ra_ur_og PEF2020.xlsx", col_names = TRUE, skip = 11)
+
+
+#Sustituir NA con valorprevio en Ramo 2015
+isNA <- as.numeric(is.na(PEF2015$RAMO))
 replacement <- fill.NAs(isNA)
 if (length(replacement)){
   which.isNA <- which(as.logical(isNA))
   to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  URBase$RAMO[to.replace] <- URBase$RAMO[replacement]
+  PEF2015$RAMO[to.replace] <- PEF2015$RAMO[replacement]
 } 
 
+
+#Sustituir NA con valorprevio en Ramo 2016
+isNA <- as.numeric(is.na(PEF2016$RAMO))
+replacement <- fill.NAs(isNA)
+if (length(replacement)){
+  which.isNA <- which(as.logical(isNA))
+  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
+  PEF2016$RAMO[to.replace] <- PEF2016$RAMO[replacement]
+} 
+
+#Sustituir NA con valorprevio en Ramo 2017
+isNA <- as.numeric(is.na(PEF2017$RAMO))
+replacement <- fill.NAs(isNA)
+if (length(replacement)){
+  which.isNA <- which(as.logical(isNA))
+  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
+  PEF2017$RAMO[to.replace] <- PEF2017$RAMO[replacement]
+} 
+
+
+#Sustituir NA con valorprevio en Ramo 2018
+isNA <- as.numeric(is.na(PEF2018$RAMO))
+replacement <- fill.NAs(isNA)
+if (length(replacement)){
+  which.isNA <- which(as.logical(isNA))
+  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
+  PEF2018$RAMO[to.replace] <- PEF2018$RAMO[replacement]
+} 
+
+
+#Sustituir NA con valorprevio en Ramo 2019
+isNA <- as.numeric(is.na(PEF2019$RAMO))
+replacement <- fill.NAs(isNA)
+if (length(replacement)){
+  which.isNA <- which(as.logical(isNA))
+  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
+  PEF2019$RAMO[to.replace] <- PEF2019$RAMO[replacement]
+} 
+
+
+#Sustituir NA con valorprevio en Ramo 2020
+isNA <- as.numeric(is.na(PEF2020$RAMO))
+replacement <- fill.NAs(isNA)
+if (length(replacement)){
+  which.isNA <- which(as.logical(isNA))
+  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
+  PEF2020$RAMO[to.replace] <- PEF2020$RAMO[replacement]
+} 
+
+View(PEF2016)
+View(PEF2015)
+View(PEF2017)
+View(PEF2018)
+View(PEF2019)
+View(PEF2020)
 
 #Sustituir NA con valorprevio en UR
 isNA <- as.numeric(is.na(URBase$UR))
@@ -86,4 +146,12 @@ write_xlsx(URBase,"ac01.xlsx")
 
 sapply(URBase, function(x) sum(is.na(x)))
 
+View(URBase)
+
+
+
+#https://www.ppef.hacienda.gob.mx/work/models/PPEF2020/analiticosPresupuestarios/Proyecto/ac01_ra_ur_og.xlsx
+#Análisis por Unidad Responsable
+URBase <- read_excel("ac01_ra_ur_og.xlsx",
+                     range = "A12:F86279")
 View(URBase)
