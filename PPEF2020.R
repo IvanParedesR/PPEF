602955f24bc78228af4be0b7d1875c4cb267b78a
Ivan Paredes <ivanresendiz25@gmail.com>
2020-08-17 18:46:26 -0500

    Local change

--- PPEF2020.R
+++ PPEF2020.R
@@ -1,40 +1,9 @@
 library(readxl)
 library(zoo)
 
-#generar función para repetir nombres
-
-last <- function (x){
-  x[length(x)]
-}    
-
-fill.NAs <- function(isNA){
-  if (isNA[1] == 1) {
-    isNA[1:max({which(isNA==0)[1]-1},1)] <- 0 # first is NAs 
-    # can't be forward filled
-  }
-  isNA.neg <- isNA.pos <- isNA.diff <- diff(isNA)
-  isNA.pos[isNA.diff < 0] <- 0
-  isNA.neg[isNA.diff > 0] <- 0
-  which.isNA.neg <- which(as.logical(isNA.neg))
-  if (length(which.isNA.neg)==0) return(NULL) # generates warnings later, but works
-  which.isNA.pos <- which(as.logical(isNA.pos))
-  which.isNA <- which(as.logical(isNA))
-  if (length(which.isNA.neg)==length(which.isNA.pos)){
-    replacement <- rep(which.isNA.pos[2:length(which.isNA.neg)], 
-                       which.isNA.neg[2:max(length(which.isNA.neg)-1,2)] - 
-                         which.isNA.pos[1:max(length(which.isNA.neg)-1,1)])      
-    replacement <- c(replacement, rep(last(which.isNA.pos), last(which.isNA) - last(which.isNA.pos)))
-  } else {
-    replacement <- rep(which.isNA.pos[1:length(which.isNA.neg)], which.isNA.neg - which.isNA.pos[1:length(which.isNA.neg)])     
-    replacement <- c(replacement, rep(last(which.isNA.pos), last(which.isNA) - last(which.isNA.pos)))
-  }
-  replacement
-}
-
 #############
 #2015 - 2020
 PEF2015 <- read_excel("2020-2016/ac01_ra_ur_og PEF2015.xlsx", col_names = TRUE, skip = 11)
-PEF2016 <- read_excel("2020-2016/ac01_ra_ur_og PEF2016.xlsx", col_names = TRUE, skip = 11)
 PEF2016 <- read_excel("2020-2016/ac01_ra_ur_og PEF2016.xlsx", col_names = TRUE, skip = 11)
 PEF2017 <- read_excel("2020-2016/ac01_ra_ur_og PEF2017.xlsx", col_names = TRUE, skip = 11)
 PEF2018 <- read_excel("2020-2016/ac01_ra_ur_og PEF2018.xlsx", col_names = TRUE, skip = 11)
@@ -42,63 +11,37 @@ PEF2019 <- read_excel("2020-2016/ac01_ra_ur_og PEF2019.xlsx", col_names = TRUE, 
 PEF2020 <- read_excel("2020-2016/ac01_ra_ur_og PEF2020.xlsx", col_names = TRUE, skip = 11)
 
 
-#Sustituir NA con valorprevio en Ramo 2015
-isNA <- as.numeric(is.na(PEF2015$RAMO))
-replacement <- fill.NAs(isNA)
-if (length(replacement)){
-  which.isNA <- which(as.logical(isNA))
-  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  PEF2015$RAMO[to.replace] <- PEF2015$RAMO[replacement]
-} 
+require(tidyverse) #fill is part of tidyr
 
+PEF2015$RAMO <- na.locf(PEF2015$RAMO) # Apply na.locf function
+PEF2015 <- PEF2015[-c(1, 1), ]
+PEF2015$UR <- na.locf(PEF2015$UR)
+PEF2015 <- PEF2015[-c(1, 1), ]
 
-#Sustituir NA con valorprevio en Ramo 2016
-isNA <- as.numeric(is.na(PEF2016$RAMO))
-replacement <- fill.NAs(isNA)
-if (length(replacement)){
-  which.isNA <- which(as.logical(isNA))
-  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  PEF2016$RAMO[to.replace] <- PEF2016$RAMO[replacement]
-} 
+PEF2016$RAMO <- na.locf(PEF2016$RAMO) # Apply na.locf function
+PEF2016 <- PEF2016[-c(1, 1), ]
+PEF2016$UR <- na.locf(PEF2016$UR)
+PEF2016 <- PEF2016[-c(1, 1), ]
 
-#Sustituir NA con valorprevio en Ramo 2017
-isNA <- as.numeric(is.na(PEF2017$RAMO))
-replacement <- fill.NAs(isNA)
-if (length(replacement)){
-  which.isNA <- which(as.logical(isNA))
-  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  PEF2017$RAMO[to.replace] <- PEF2017$RAMO[replacement]
-} 
+PEF2017$RAMO <- na.locf(PEF2017$RAMO) # Apply na.locf function
+PEF2017 <- PEF2017[-c(1, 1), ]
+PEF2017$UR <- na.locf(PEF2017$UR)
+PEF2017 <- PEF2017[-c(1, 1), ]
 
+PEF2018$RAMO <- na.locf(PEF2018$RAMO) # Apply na.locf function
+PEF2018 <- PEF2018[-c(1, 1), ]
+PEF2018$UR <- na.locf(PEF2018$UR)
+PEF2018 <- PEF2018[-c(1, 1), ]
 
-#Sustituir NA con valorprevio en Ramo 2018
-isNA <- as.numeric(is.na(PEF2018$RAMO))
-replacement <- fill.NAs(isNA)
-if (length(replacement)){
-  which.isNA <- which(as.logical(isNA))
-  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  PEF2018$RAMO[to.replace] <- PEF2018$RAMO[replacement]
-} 
+PEF2019$RAMO <- na.locf(PEF2019$RAMO) # Apply na.locf function
+PEF2019 <- PEF2019[-c(1, 1), ]
+PEF2019$UR <- na.locf(PEF2019$UR)
+PEF2019 <- PEF2019[-c(1, 1), ]
 
-
-#Sustituir NA con valorprevio en Ramo 2019
-isNA <- as.numeric(is.na(PEF2019$RAMO))
-replacement <- fill.NAs(isNA)
-if (length(replacement)){
-  which.isNA <- which(as.logical(isNA))
-  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  PEF2019$RAMO[to.replace] <- PEF2019$RAMO[replacement]
-} 
-
-
-#Sustituir NA con valorprevio en Ramo 2020
-isNA <- as.numeric(is.na(PEF2020$RAMO))
-replacement <- fill.NAs(isNA)
-if (length(replacement)){
-  which.isNA <- which(as.logical(isNA))
-  to.replace <- which.isNA[which(isNA==0)[1]:length(which.isNA)]
-  PEF2020$RAMO[to.replace] <- PEF2020$RAMO[replacement]
-} 
+PEF2020$RAMO <- na.locf(PEF2020$RAMO) # Apply na.locf function
+PEF2020 <- PEF2020[-c(1, 1), ]
+PEF2020$UR <- na.locf(PEF2020$UR)
+PEF2020 <- PEF2020[-c(1, 1), ]
 
 View(PEF2016)
 View(PEF2015)
